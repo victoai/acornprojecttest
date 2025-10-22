3729ab0b59a8ff72aee0aa4f19036cc08768e349
victoai <victoai@naver.com>
2025-10-22 14:39:07 +0900

    dfdf

--- test/src/main/java/test/dt.java
+++ test/src/main/java/test/dt.java
@@ -1,5 +1,19 @@
 package test;
 
-public class dt {
+import java.io.IOException;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebListener("/aa")
+public class dt extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		 
+	}
 }
